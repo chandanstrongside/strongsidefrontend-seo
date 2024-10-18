ddd434ccd3e9fdd7fd9f52e829a11f192b1041fe
Akanksha Konar <akanksha.konar@qbent.in>
2024-10-18 11:47:58 +0530

    New Project With SSR

---
+++ src/app/plays/plays.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-plays',
+  standalone: true,
+  imports: [],
+  templateUrl: './plays.component.html',
+  styleUrl: './plays.component.css'
+})
+export class PlaysComponent {
+
+}
